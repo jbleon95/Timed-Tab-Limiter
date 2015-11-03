d7e90b15f715917d161ed6c7da7645f86603dec3
Jeremy Leon <jeremybenleon@gmail.com>
2015-11-02 22:42:30 -0500

    Options check for valid input

--- options.js
+++ options.js
@@ -12,15 +12,35 @@ function loadOptions() {
 
 function saveOptions() {
     var time = +document.getElementById("time").value;
-    chrome.storage.sync.set({"timerLength": time}, function() {
-        console.log(time);
+    if (isInteger(time) && time > 0){
+        chrome.storage.sync.set({"timerLength": time}, function() {
+            console.log(time);
+            var status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(function() {
+                status.textContent = '';
+            }, 750);
+      });
+    }
+    else if (isInteger(time)) {
         var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
+        status.textContent = 'Timer Length must be greater than zero.';
         setTimeout(function() {
             status.textContent = '';
-        }, 750);
-  });
+        }, 1000);
+    }
+    else {
+        var status = document.getElementById('status');
+        status.textContent = 'Not an Integer.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 1000);
+    }
 }
+
+function isInteger(x) {
+        return (typeof x === 'number') && (x % 1 === 0);
+    }
 
 document.addEventListener('DOMContentLoaded', loadOptions);
 document.getElementById('save').addEventListener('click',
